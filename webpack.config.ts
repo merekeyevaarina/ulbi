ae38627465b2902799ffba1e8ac3b227a26b1001
Arinella <jumabaevaarina@gmail.com>
2026-08-27 17:32:59 +0500

    bug fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import webpack from "webpack";
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import path from "path";
 import {BuildEnv, BuildPath} from "./config/build/types/config";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 
 
 export default (env: BuildEnv) => {
